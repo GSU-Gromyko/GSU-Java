2390706320e42a421ab1ed185f62092d00699bb4
VItya Gromyko <vitygromyko@mail.ru>
2020-02-06 16:15:21 +0300

    1е задания

--- src/Task_1.java
+++ src/Task_1.java
@@ -6,16 +6,22 @@ public class Task_1 {
 
         class Runner{
 
-            // a - x
-            // h - step
-            // n - count
-            float a, h;
+            // a - x, h - step, n - count
+            // m - ограничение для 2й части задания
+            float a, h, m;
             int n;
 
-            Runner(float x, float step, int count){
+            public Runner(float x, float step, int count){
                 a = x;
                 h = step;
                 n = count;
+            }
+
+            public Runner(float x, float step, int count, float limit) {
+                a = x;
+                h = step;
+                n = count;
+                m = limit;
             }
 
             float func(float x){
@@ -24,16 +30,27 @@ public class Task_1 {
 
             void get_results(){
                 for (int i = 0; i < n; i++) {
-                    System.out.println(func(a + h * i));
+                    float value = a + h * i;
+                    if (m != 0 && value > m){
+                        System.out.println("Х вышел за область установленных значений");
+                    } else {
+                        System.out.println(func(value));
+                    }
                 }
             }
         }
 
+
         Scanner in = new Scanner(System.in);
 
+
         System.out.println("Введите Х, шаг и количество итераций");
-        Runner run = new Runner(in.nextFloat(), in.nextFloat(), in.nextInt());
-        run.get_results();
+        Runner part_1 = new Runner(in.nextFloat(), in.nextFloat(), in.nextInt());
+        part_1.get_results();
+
+        System.out.println("Введите Х, шаг, количество итераций и ограничитель");
+        Runner part_2 = new Runner(in.nextFloat(), in.nextFloat(), in.nextInt(), in.nextFloat());
+        part_2.get_results();
 
     }
 }
