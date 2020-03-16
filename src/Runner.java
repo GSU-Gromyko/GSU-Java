e02ae037c92e5c2c324aacdf31b7c2b470b3ca31
VItya Gromyko <vitygromyko@mail.ru>
2020-03-16 18:25:09 +0300

    Practice 2

---
+++ src/Runner.java
@@ -0,0 +1,25 @@
+import by.gsu.ms32lab.Detail;
+import by.gsu.ms32lab.Material;
+
+public class Runner {
+    public static void main(String[] args) {
+
+        Material steel = new Material("Steel", 7800);
+        Material copper = new Material("copper", 8500);
+
+        // 1
+        Detail wire = new Detail("Wire", steel, 0.03);
+
+        // 2
+        System.out.println(wire.toString());
+
+        //3
+        wire.setMaterial(copper);
+        System.out.println(wire.getMass());
+
+        //4
+        wire.setMaterial(steel);
+        System.out.println(wire.toString());
+
+    }
+}
---
+++ src/by/gsu/ms32lab/Material.java
@@ -0,0 +1,39 @@
+package by.gsu.ms32lab;
+
+public class Material {
+    private String name;
+    private int density;
+
+    public Material() {
+        this.name = "Material";
+        this.density = 1000;
+    }
+
+    public Material(String name, int density) {
+        this.name = name;
+        this.density = density;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getDensity() {
+        return density;
+    }
+
+    public void setDensity(int density) {
+        this.density = density;
+    }
+
+    public String toString() {
+
+        return name + ";" + density;
+    }
+
+
+}
